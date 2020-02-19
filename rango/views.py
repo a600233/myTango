a04cd0f6f4e2778a8de415ad5b0fc33dc55b90f5
Wujiiuge Yin <ywjg_99@126.com>
2020-02-19 12:05:37 +0000

    cpter 9

--- rango/views.py
+++ rango/views.py
@@ -5,8 +5,12 @@ from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
 from rango.forms import PageForm
-from django.shortcuts import redirect
+from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
 from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def about(request):
     # prints out whether the method is a GET or a POST
@@ -70,7 +74,7 @@ def add_category(request):
             cat = form.save(commit=True)
             print(cat, cat.slug)
 # Now that the category is saved, we could confirm this. # For now, just redirect the user back to the index view.
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             # just print them to the terminal.
             print(form.errors)
@@ -86,7 +90,7 @@ def add_page(request, category_name_slug):
 
 # You cannot add a page to a Category that does not exist...
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
@@ -101,5 +105,88 @@ def add_page(request, category_name_slug):
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
             print(form.errors)
+
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            # until we're ready to avoid integrity problems.
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            # Did the user provide a profile picture?
+            # If so, we need to get it from the input form and #put it in the UserProfile model.
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']  # Now we save the UserProfile model instance.
+            profile.save()
+        # Update our variable to indicate that the template # registration was successful.
+            registered = True
+        else:
+        # Invalid form or forms - mistakes or something else? # Print problems to the terminal.
+            print(user_form.errors, profile_form.errors)
+    else:
+        # Not a HTTP POST, so we render our form using two ModelForm instances. # These forms will be blank, ready for user input.
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+        # Render the template depending on the context.
+    return render(request,
+                  'rango/register.html',
+                context={'user_form': user_form,
+                        'profile_form': profile_form,
+                        'registered': registered})
+def user_login(request):
+# If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        # We use request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+# request.POST.get('<variable>') returns None if the
+# value does not exist, while request.POST['<variable>'] # will raise a KeyError exception.
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+# Use Django's machinery to attempt to see if the username/password # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+# If we have a User object, the details are correct.
+# If None (Python's way of representing the absence of a value), no user # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+# If the account is valid and active, we can log the user in. # We'll send the user back to the homepage.
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+# An inactive account was used - no logging in!
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+# Bad login details were provided. So we can't log the user in.
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    # The request is not a HTTP POST, so display the login form.
+    else:
+# No context variables to pass to the template system, hence the # blank dictionary object...
+        return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+# Use the login_required() decorator to ensure only those logged in can # access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
--- tango_with_django_project_2487780Y/settings.py
+++ tango_with_django_project_2487780Y/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -101,6 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
 ]
 
@@ -123,6 +133,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+LOGIN_URL = 'rango:login'
